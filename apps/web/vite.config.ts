76288e9fc013d7a4a5c6f53a0fa45985d38d2e64
SREEHARIkj <sreeharikj8@gmail.com>
2024-02-25 22:43:03 +0530

    feat(api/web) Dockerization

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,13 +1,26 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import path from 'path';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    define: {
+      'process.env': env,
     },
-  },
+    server: {
+      host: true,
+    },
+    preview: {
+      port: 5000,
+      host: true,
+    },
+    plugins: [react()],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+      },
+    },
+  };
 });
